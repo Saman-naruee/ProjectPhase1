682c1e6d588b115055b3eae9628993ca1be05fd9
Saman-naruee <samannaruee@gmail.com>
2024-08-13 12:56:52 +0330

    customize User model's admin panel with filter and list_display and also fieldsets

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 from .models import User
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',)
+    list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        ('',{
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description'),
+        }),
+        ('Contact info', {
+            'fields': ('phone', 'address'),
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
