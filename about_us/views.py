493bdc0ff0eddb55e5ea00cc8ed2fdaf6c859b71
Saman-naruee <samannaruee@gmail.com>
2024-08-17 21:16:40 +0330

    update about_us.html and views.py of about_us app to show team_members

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render
+from accounts.models import User
+
 def about_us(request):
-    # View for about-us page
-    ...
+    data = User.objects.all()
+    context = {
+        'users': data
+    }
+    return render(request, 'about_us.html', context)
