deae6c986f76cc6f40d76325075c5507a1a3d07e
Saman-naruee <samannaruee@gmail.com>
2024-08-18 22:29:58 +0330

    add list_editable in accounts/admin.py for is_active, is_staff.

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',)
     list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active')
+    list_editable = ['is_active', 'is_staff']
     fieldsets = (
         ('',{
             'fields': ('username', 'password')
