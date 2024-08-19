6cff7019fbdb6c2950d0afc1941b2788b8987155
Saman-naruee <samannaruee@gmail.com>
2024-08-19 23:31:30 +0330

    add tesk manager to charities models in main branch

--- charities/admin.py
+++ charities/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Task, Benefactor, Charity
 
-admin.site.register(Task)
-admin.site.register(Benefactor)
 admin.site.register(Charity)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'state', 'gender_limit')
+    list_editable = ('state', 'gender_limit')
+    list_display_links = ('title',)
+
+
+@admin.register(Benefactor)
+class BenefactorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'experience')
+    list_editable = ('experience',)
+    list_display_links = ('user',)
--- charities/models.py
+++ charities/models.py
@@ -14,6 +14,9 @@ class Benefactor(models.Model):
     experience = models.SmallIntegerField(choices = EXPERIENCE_CHOICES, default = 1)
     free_time_per_week = models.PositiveSmallIntegerField(default = 0)
 
+    def __str__(self) -> str:
+        return f'benefactor for : {self.user.username}'
+
 
 class Charity(models.Model):
     # id = models.AutoField(primary_key = True)
@@ -21,6 +24,19 @@ class Charity(models.Model):
     name = models.CharField(max_length = 50)
     reg_number = models.CharField(max_length = 10, default = '')
 
+    def __str__(self) -> str:
+        return f'charity for : {self.user.username}'
+
+
+class TaskManager(models.Manager):
+    def related_tasks_to_charity(self, user):
+        return self.none() if (Charity.DoesNotExist or not user) else self.filter(charity = Charity.objects.get(user=user))
+
+    def related_tasks_to_benefactor(self, user):
+        return self.none() if (Benefactor.DoesNotExist or not user) else self.filter(assigned_benefactor = Benefactor.objects.get(user=user))
+
+    def all_related_tasks_to_user(self, user):
+        return self.related_tasks_to_benefactor(user) and self.related_tasks_to_charity(user)
 
 class Task(models.Model):
     Gender_Choices = [
@@ -45,3 +61,9 @@ class Task(models.Model):
     title = models.CharField(max_length = 60)
     state = models.CharField(max_length = 1, choices = States, default = 'P')
     gender_limit = models.CharField(max_length = 1, choices = Gender_Choices)
+
+    objects = models.Manager()
+    task_manager = TaskManager()
+
+    def __str__(self) -> str:
+        return f'Task{self.pk}: {self.title}'
